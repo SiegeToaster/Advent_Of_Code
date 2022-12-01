80f4d7036b0ad554f122f15d2b00368c73a6bc74
Siege <81945479+SiegeToaster@users.noreply.github.com>
2022-11-30 21:13:52 -0800

    2022 day 1

---
+++ 2022/day1/part2.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const raw = fs.readFileSync("2022/day1/input.txt", "utf-8");
+const instructions = raw.split('\r\n');
+let elves = [0];
+let currentElf = 0;
+
+for (const instruction of instructions) {
+	if (instruction == '') {
+		currentElf++;
+		elves[currentElf] = 0
+	} else {
+		elves[currentElf] = elves[currentElf] + parseInt(instruction);
+	}
+}
+
+elves.sort((a, b) => {
+	return b - a;
+});
+
+console.log(elves[0] + elves[1] + elves[2]);
